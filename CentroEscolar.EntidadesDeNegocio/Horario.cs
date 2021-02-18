bcde9f579408325fbe8a87179014ae5bfbf8a456
Lennin5 <lenninlemus0@gmail.com>
2021-02-18 14:09:40 -0600

    se agrego la clase Horario

---
+++ CentroEscolar.EntidadesDeNegocio/Horario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentroEscolar.EntidadesDeNegocio
+{
+    class Horario
+    {
+        public int Id { get; set; }
+
+        public string HorarioDeClase { get; set; }
+
+        public DateTime HoraEntrada { get; set; }
+
+        public DateTime HoraSalida { get; set; }
+
+    }
+}
