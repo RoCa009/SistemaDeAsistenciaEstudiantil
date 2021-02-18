172c194009d51ffd4abf363fe007fe73b25b4023
Rodrigo A. Caballero <rcaballero082@gmail.com>
2021-02-18 14:13:36 -0600

    Se agrega la Clase Grado, faltan campos

---
+++ CentroEscolar.EntidadesDeNegocio/Grado.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentroEscolar.EntidadesDeNegocio
+{
+    public class Grado
+    {
+        public int Id { get; set; }
+    }
+}
