d313ead42cece9f4b08c9197588c02e36893184d
Rodrigo A. Caballero <rcaballero082@gmail.com>
2021-02-22 19:21:04 -0600

    Commit con todas las clases del proyecto

---
+++ CentroEscolar.EntidadesDeNegocio/Profesor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentroEscolar.EntidadesDeNegocio
+{
+    public class Profesor
+    {
+        public int Id { get; set; }
+        public string DUIEncargado{ get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public int Edad { get; set; }
+        public string Sexo { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+    }
+}
