9362c246f4e2bba221957207abaa75bdfa4e2c46
Rodrigo A. Caballero <rcaballero082@gmail.com>
2021-02-24 18:07:26 -0600

    Se hacen arreglos en GradoDAL, Grado.cs y AulaDAL

---
+++ CentroEscolar.AccesoADatos/AulaDAL.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+using CentroEscolar.EntidadesDeNegocio;
+
+namespace CentroEscolar.AccesoADatos
+{
+    public class AulaDAL
+    {
+        // Metodo Leer
+        public List<Aula> Obtener()
+        {
+            List<Aula> ListaAulas = new List<Aula>();
+            using (SqlConnection con = Conexion.Conectar())
+            {
+                con.Open();
+                string ssql = "select * from aulas";
+                SqlCommand comando = new SqlCommand(ssql, con);
+                comando.CommandType = CommandType.Text;
+                IDataReader reader = comando.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    ListaAulas.Add(new Aula(reader.GetInt32(0), reader.GetInt32(1)));
+                }
+
+                con.Close();
+            }
+
+            return ListaAulas;
+        }
+
+        //Metodo Agregar
+        public int Agregar(Aula pAula)
+        {
+            int resultado = 0;
+            using (SqlConnection con = Conexion.Conectar())
+            {
+                con.Open();
+                string sentencia = "insert into aulas(id, numerodeaula) values('{0}', '{1}')";
+                string ssql = string.Format(sentencia, pAula.Id, pAula.NumeroDeAula);
+                SqlCommand comando = new SqlCommand(ssql, con);
+                comando.CommandType = CommandType.Text;
+                resultado = comando.ExecuteNonQuery();
+
+                con.Close();
+            }
+
+            return resultado;
+        }
+
+        //Metodo Modificar
+        public int Modificar(Aula pAula)
+        {
+            int resultado = 0;
+            using (SqlConnection con = Conexion.Conectar())
+            {
+                con.Open();
+                string sentencia = "update Aulas set numerodeaula='{1}' where id={0} ";
+                string ssql = string.Format(sentencia, pAula.Id, pAula.NumeroDeAula);
+                SqlCommand comando = new SqlCommand(ssql, con);
+                comando.CommandType = CommandType.Text;
+                resultado = comando.ExecuteNonQuery();
+
+                con.Close();
+            }
+
+            return resultado;
+        }
+
+        //Metodo Eliminar
+        public int Eliminar(int pId)
+        {
+            int resultado = 0;
+            using (SqlConnection con = Conexion.Conectar())
+            {
+                con.Open();
+                string sentencia = "delete from aulas where id={0} ";
+                string ssql = string.Format(sentencia, pId);
+                SqlCommand comando = new SqlCommand(ssql, con);
+                comando.CommandType = CommandType.Text;
+                resultado = comando.ExecuteNonQuery();
+
+                con.Close();
+            }
+
+            return resultado;
+        }
+
+        // Metodo BuscarPorId
+        public static Aula BuscarPorId(int pId)
+        {
+            Aula Aula = new Aula();
+            using (SqlConnection con = Conexion.Conectar())
+            {
+                con.Open();
+                string sentencia = "select * from aulas where id={0}";
+                string ssql = string.Format(sentencia, pId);
+                SqlCommand comando = new SqlCommand(ssql, con);
+                comando.CommandType = CommandType.Text;
+                IDataReader reader = comando.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    Aula.Id = reader.GetInt32(0);
+                    Aula.NumeroDeAula = reader.GetInt32(1);
+                }
+
+                con.Close();
+            }
+
+            return Aula;
+        }
+    }
+}
